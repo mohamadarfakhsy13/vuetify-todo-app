f25cb35932f1f33cf9f82beb57919c0c7dd4e364
arfakhsy <mohamad.arfakhsy13@gmail.com>
2022-07-14 16:59:01 +0800

    Add method delete

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -8,18 +8,33 @@
       v-for="task in tasks"
       :key="task.id"
       >
-        <v-list-item>
-          <template v-slot:default="{ active, }">
+        <v-list-item 
+          @click="doneTask(task.id)"
+          :class="{ 'teal lighten-5' : task.done }"
+          >
+          <template v-slot:default>
             <v-list-item-action>
               <v-checkbox
-                :input-value="active"
+                :input-value="task.done"
                 color="primary"
               ></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title>{{ task.title }}</v-list-item-title>
+              <v-list-item-title
+                class=" { text-decoration-line-through = task.done }"
+              >
+                {{ task.title }}
+              </v-list-item-title>
             </v-list-item-content>
+            <v-list-item-action>
+              <v-btn
+              @click.stop="deleteTask(task.id)" 
+                icon
+              >
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
           </template>
         </v-list-item>
       <v-divider></v-divider>
@@ -29,8 +44,6 @@
 </template>
 
 <script>
-import { upperFirst } from 'vuetify/lib/util/helpers'
-
 
   export default {
     name: 'Home',
@@ -39,18 +52,31 @@ import { upperFirst } from 'vuetify/lib/util/helpers'
         tasks: [
           {
             id: 1,
-            title: 'Wake upper'
+            title: 'Wake upper',
+            done: 'false'
           },
           {
             id: 2,
-            title: 'Do push up'
+            title: 'Do push up',
+            done: 'false'
           },
           {
             id: 3,
-            title: 'Eat breakfast'
+            title: 'Eat breakfast',
+            done: 'false'
           }
         ]
       }
-    }
+    },
+    methods: {
+        doneTask(id) {
+          let task = this.tasks.filter(task => task.id === id)[0]
+          task.done = !task.done
+          console.log('id:', id)
+        },
+        deleteTask(id) {
+          this.tasks = this.tasks.filter(task => task.id !== id)
+        }
+      }
   }
 </script>
